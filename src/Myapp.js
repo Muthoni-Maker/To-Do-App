16d01c5cc4c597b09100d8b8c5c4926c3d814de4
Muthoni-Maker <kamauesthermuthoni@gmail.com>
2020-07-25 07:27:36 +0300

    my first code push

---
+++ src/Myapp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ToDoItem from './To-do-item.js';
+
+function Myapp(){
+    return (
+        <div className="New-list">
+           <ToDoItem/>
+           <ToDoItem/>
+           <ToDoItem/>
+           <ToDoItem/>
+           <ToDoItem/>
+         
+        </div>
+    );
+}
+export default Myapp;
---
+++ src/To-do-item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ToDoItem(){
+    return (
+        <div className="Todo-items">
+        <input type="checkbox"/>
+        <p>Placeholder Here</p>
+        </div>
+    );
+}
+export default ToDoItem;
